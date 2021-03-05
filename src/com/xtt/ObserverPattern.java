6dd6e6945125210e6ba10f6d11872396bd7c08d2
huMonster <monster.hu@outlook.com>
2021-03-05 22:41:57 +0800

    观察者模式示例

--- src/com/xtt/ObserverPattern.java
+++ src/com/xtt/ObserverPattern.java
@@ -1,35 +1,106 @@
 package com.xtt;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
 /**
- * 抽象观察者
+ * Subject：公众号->推送文章
  *
  */
-interface Observer{
-    public void update(String message);
+interface IOfficialAccount{
+    /**
+     * 推送文章
+     */
+    void notifySubscribers();
+
+    /**
+     * 添加订阅
+     *
+     * @param user
+     */
+    void addSubscribe(User user);
+
+    /**
+     * 取消订阅
+     *
+     * @param user
+     */
+    void cancelSubscribe(User user);
+}
+/**
+ * concreteSubject: 具体的某一个公众号
+ *             实现公众号接口的三个方法
+ */
+
+class ConcreteOfficialAccount implements IOfficialAccount{
+
+    private ArrayList<User> list;
+    private String article;
+
+    public ConcreteOfficialAccount() {
+        list = new ArrayList<>();
+    }
+
+    @Override
+    public void notifySubscribers() {
+        // 通知所有订阅者
+        for (User user : list) {
+            user.update(article);
+        }
+    }
+
+    @Override
+    public void addSubscribe(User user) {
+        list.add(user);
+    }
+
+    @Override
+    public void cancelSubscribe(User user) {
+        if(!list.isEmpty()){
+            list.remove(user);
+        }
+    }
+
+    /**
+     * 推送的文章
+     *
+     * @param article
+     */
+    public void pushArticle(String article) {
+        this.article = article;
+        notifySubscribers();
+    }
 }
 
 /**
- * 具体观察者
+ * Observer: 观察者接口
+ * 定义了一个update()方法，当被观察者调用pushArticle(String article)方法时，观察者的update()方法会被回调。
  */
-class User implements Observer{
+interface IUser{
+    /**
+     * 接收更新文章
+     *
+     * @param message
+     */
+    void update(String message);
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class User implements IUser{
 
     private String name;
-    private String message;
 
     @Override
     public void update(String message) {
-
+        System.out.println(name + "有一个新的推送文章：" + message);
     }
 }
-/**
- * 抽象主题
- * 声明了添加、删除、通知观察者的方法
- */
-interface Subject{
-    public void registerObserver(Observer o);
-    public void removeObserver(Observer o);
-    public void notifyObserver();
-}
+
 /**
  * @Description 观察者模式：在对象之间定义一对多的依赖，当一个对象状态改变，会通知其他对象收到通知并自动更新
  *                实际运用：springboot启动原理中 应用启动监听器模块（SpringApplicationRunListeners）
@@ -52,6 +123,24 @@ public class ObserverPattern {
      * @param args
      */
     public static void main(String[] args) {
+        ConcreteOfficialAccount c = new ConcreteOfficialAccount();
+        // 两个用户
+        User u = new User("张三");
+        User u2 = new User("李四");
 
+        // 订阅了c公众号
+        c.addSubscribe(u);
+        c.addSubscribe(u2);
+
+        // c公众号推送一篇文章
+        c.pushArticle("哆啦A梦");
+
+        // 用户u取消订阅
+        c.cancelSubscribe(u);
+
+        // c公众号再推送一篇文章
+        c.pushArticle("金刚葫芦娃");
+
+        // 打印结果为哪些用户接受到了公众号的文章
     }
 }
